ce4b5d141be1e11a46d9015b93a282e8ff572033
Cauã Vinícius <112443019+cauavsb@users.noreply.github.com>
2023-08-29 07:57:21 -0300

    atualizei

--- faculdade/arvore_binaria_2/ArvoreBinaria.java
+++ faculdade/arvore_binaria_2/ArvoreBinaria.java
@@ -5,14 +5,120 @@ public class ArvoreBinaria<T> {
         this.raiz = null;
     }
 
-    public int alturaArvore(No<T> atual) {
-        if (atual == null) {
-            return -1;
+    public void adiciona(int info) {
+        No<T> novoNo = new No<>(info);
+        if (arvoreVazia()) {
+            raiz = novoNo;
+            return;
         }
         else {
-            int alturaEsquerda = alturaArvore(atual.getEsquerda());
-            int alturaDireita = alturaArvore(atual.getDireita());
-            return Math.max(alturaEsquerda, alturaDireita) + 1;
+            No<T> atual = raiz;
+            while (true) {
+                if (novoNo.getInfo() < atual.getInfo()) {
+                    if (atual.getEsquerda() != null) {
+                        atual = atual.getEsquerda();
+                    }
+                    else {
+                        atual.setEsquerda(novoNo);
+                        break;
+                    }
+                }
+                else {
+                    if (atual.getDireita() != null) {
+                        atual = atual.getDireita();
+                    }
+                    else {
+                        atual.setDireita(novoNo);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public void remove(int info) {
+        if (pertenceArvore(info)) {
+            No<T> paiAtual = null;
+            No<T> atual = raiz;
+            while (atual != null) {
+                if (info == atual.getInfo()) {
+                    break;
+                }
+                else {
+                    if (info < atual.getInfo()) {
+                        paiAtual = atual;
+                        atual = atual.getEsquerda();
+                    }
+                    else {
+                        paiAtual = atual;
+                        atual = atual.getDireita();
+                    }
+                }
+            }
+
+            if (atual.getDireita() == null && atual.getEsquerda() == null) {
+                if (paiAtual == null) {
+                    raiz = null;
+                }
+                else if (atual.getInfo() < paiAtual.getInfo()) {
+                    paiAtual.setEsquerda(null);
+                }
+                else {
+                    paiAtual.setDireita(null);
+                }
+            }
+            else if (atual.getDireita() != null && atual.getEsquerda() == null || atual.getDireita() == null && atual.getEsquerda() != null) {
+               if (atual == raiz) {
+                    if (raiz.getEsquerda() == null) {
+                        raiz = raiz.getDireita();
+                    }
+                    else {
+                        raiz = raiz.getEsquerda();
+                    }
+                }
+                else if (atual.getInfo() < paiAtual.getInfo()) {
+                    if (atual.getEsquerda() != null) {
+                        paiAtual.setEsquerda(atual.getEsquerda());
+                    }
+                    else {
+                        paiAtual.setEsquerda(atual.getDireita());
+                    }
+                }
+                else {
+                    if (atual.getEsquerda() != null) {
+                        paiAtual.setDireita(atual.getEsquerda());
+                    }
+                    else {
+                        paiAtual.setDireita(atual.getDireita());
+                    }
+                }
+            }
+            else {
+                No<T> paiSucessor = atual;
+                No<T> sucessor = atual.getDireita();
+                while (sucessor.getEsquerda() != null) {
+                    paiSucessor = sucessor;
+                    sucessor = sucessor.getEsquerda();
+                }
+                    
+                atual.setInfo(sucessor.getInfo());
+        
+                if (paiSucessor == atual) { // Caso o sucessor seja o filho à direita do nó atual
+                    paiSucessor.setDireita(sucessor.getDireita());
+                    //if (procuraElemento(info)) {
+                        //remove(info);
+                    //}
+                } 
+                else {
+                    paiSucessor.setEsquerda(sucessor.getDireita());
+                    //if (procuraElemento(info)) {
+                        //remove(info);
+                    //}
+                }
+            }
+        }
+        else {
+            System.out.println("Elemento não está presente na Ávore!");
         }
     }
 
@@ -35,6 +141,17 @@ public class ArvoreBinaria<T> {
             }
         }
         return false;
+    }
+
+    public int alturaArvore(No<T> atual) {
+        if (atual == null) {
+            return -1;
+        }
+        else {
+            int alturaEsquerda = alturaArvore(atual.getEsquerda());
+            int alturaDireita = alturaArvore(atual.getDireita());
+            return Math.max(alturaEsquerda, alturaDireita) + 1;
+        }
     }
 
     public void preOrdem(No<T> atual) {
@@ -60,6 +177,50 @@ public class ArvoreBinaria<T> {
             System.out.println(atual.getInfo());
         }
     }
+
+    public Boolean arvoreCheia(No<T> atual) {
+        if (atual == null) {
+            return false;
+        }
+        
+        if (atual.getEsquerda() == null && atual.getDireita() == null) {
+            return true;
+        }
+    
+        if (atual.getEsquerda() != null && atual.getDireita() != null) {
+            return arvoreCheia(atual.getEsquerda()) && arvoreCheia(atual.getDireita());
+        }
+    
+        return false;
+    }
+
+    public Boolean arvoreCompleta(No<T> atual) {
+        if (atual == null) {
+            return true;
+        }
+        
+        int alturaEsquerda = alturaArvore(atual.getEsquerda());
+        int alturaDireita = alturaArvore(atual.getDireita());
+    
+        if (Math.abs(alturaEsquerda - alturaDireita) <= 1 &&
+            arvoreCompleta(atual.getEsquerda()) && arvoreCompleta(atual.getDireita())) {
+            return true;
+        }
+    
+        return false;
+    }
+
+    public Boolean arvoreDegenerada(No<T> atual) {
+        if (atual == null) {
+            return true;
+        }
+    
+        if (atual.getEsquerda() != null && atual.getDireita() != null) {
+            return false;
+        }
+
+        return arvoreDegenerada(atual.getDireita()) || arvoreDegenerada(atual.getEsquerda());
+    }    
 
     public Boolean arvoreVazia() {
         return raiz == null;
--- faculdade/arvore_binaria_2/TesteArvoreBinaria.java
+++ faculdade/arvore_binaria_2/TesteArvoreBinaria.java
@@ -1,5 +1,29 @@
 public class TesteArvoreBinaria {
     public static void main(String[] args) {
-        
+        ArvoreBinaria<Integer> arvoreBinaria = new ArvoreBinaria<>();
+        arvoreBinaria.adiciona(10);
+        arvoreBinaria.adiciona(11);
+        arvoreBinaria.adiciona(10);
+        arvoreBinaria.adiciona(18);
+        arvoreBinaria.adiciona(47);
+        arvoreBinaria.adiciona(5);
+        arvoreBinaria.adiciona(3);
+        arvoreBinaria.adiciona(1);
+        arvoreBinaria.adiciona(7);
+        arvoreBinaria.adiciona(6);
+        arvoreBinaria.adiciona(9);
+        System.out.println("Raiz da árvore: " + arvoreBinaria.getRaiz().getInfo());
+        System.out.print("Em ordem: ");
+        arvoreBinaria.emOrdem(arvoreBinaria.getRaiz());
+        System.out.println();
+
+        arvoreBinaria.remove(1);
+        arvoreBinaria.remove(6);
+        arvoreBinaria.remove(9);
+        arvoreBinaria.remove(47);
+
+        System.out.println(arvoreBinaria.arvoreCheia(arvoreBinaria.getRaiz()));
+        System.out.println(arvoreBinaria.arvoreCompleta(arvoreBinaria.getRaiz()));
+        System.out.println(arvoreBinaria.arvoreDegenerada(arvoreBinaria.getRaiz()));
     }
 }
