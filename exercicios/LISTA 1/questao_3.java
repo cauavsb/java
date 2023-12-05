f73c34589d7d7439adcb55b5181236b0e0202b06
Cauã Vinícius <112443019+cauavsb@users.noreply.github.com>
2023-12-05 11:17:59 -0300

    atualizei

--- exercicios/LISTA 1/questao_3.java
+++ exercicios/LISTA 1/questao_3.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class questao_3 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-        int i=0;
-        System.out.println("Esses são todos os números pares entre 0 e 30:");
-        for(i=0;i<=30;i+=2) {
+        int i = 0;
+        System.out.println("Esses são todos os números pares entre 0 e 30: ");
+        for(i = 0; i <= 30; i += 2) {
             System.out.println(i);
         }
     }		
